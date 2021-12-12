04f4eb98fc4f9f1f3371d52d0636f3c0033404e9
ifndev <contact@patrickconti.fr>
2021-12-12 18:00:04 +0100

    options to choose method and include a custom body

--- src/index.mjs
+++ src/index.mjs
@@ -1,4 +1,4 @@
-import {fetch, CookieJar} from "node-fetch-cookies";
+import { fetch, CookieJar } from "node-fetch-cookies";
 
 export default class Session {
 
@@ -21,13 +21,13 @@ export default class Session {
             "headers": this.#baseHeaders,
             "method": "GET"
         }).catch(err => {
-            throw(`Error while fetching /login: \n${err}`);
+            throw (`Error while fetching /login: \n${err}`);
         });
-    
+
         let text = await response.text();
-    
+
         const execution = text.match(/<input type="hidden" name="execution" value="(.*?)"/)[1];
-    
+
         return execution;
     }
 
@@ -38,46 +38,59 @@ export default class Session {
      */
     async login(username, password) {
         let execution = await this.getExecution();
-    
+
         let response = await fetch(this.#sessionCookies, "https://auth.univ-lorraine.fr/login", {
             "credentials": "include",
             "headers": this.#baseHeaders,
             "method": "POST",
             "body": `username=${username}&password=${password}&execution=${execution}&_eventId=submit&submit=Connexion`
         }).catch(err => {
-            throw(`Error while logging in: \n${err}`);
+            throw (`Error while logging in: \n${err}`);
         });
-    
+
         if (response.status === 200) {
             if (response.headers.get("Set-Cookie").includes("TGC-CAS")) {
                 return;
             }
             else {
-                throw("Login error (unknown)");
+                throw ("Login error (unknown)");
             }
         } else {
-            throw("Login error (bad credentials)");
+            throw ("Login error (bad credentials)");
         }
     }
 
     async fetchPage(url, options) {
+
         if (this.#sessionCookies.cookies.get("auth.univ-lorraine.fr").get("TGC-CAS") != undefined) {
-            let response = await fetch(this.#sessionCookies, url, {
-                "headers": options?.headers != null ? {...this.#baseHeaders, ...options.headers} : this.#baseHeaders,
-                "method": "GET",
-            }).catch(err => {
-                throw(`Error while fetching ${url}: \n${err}`);
+            options = {
+                "headers": options?.headers != null ? { ...this.#baseHeaders, ...options.headers } : this.#baseHeaders,
+                "method": options?.method != null ? options.method.toUpperCase() : "GET",
+            }
+
+            if (options.body != null) {
+
+                if (options.method != "head" || options.method != "get") {
+                    options["body"] = options.body;
+                }
+                else {
+                    throw ("Cannot send a body with a GET or HEAD request");
+                }
+            }
+
+            let response = await fetch(this.#sessionCookies, url,).catch(err => {
+                throw (`Error while fetching ${url}: \n${err}`);
             });
 
             if (response.status === 440) {
-                throw("Session expired: please try logging-in again");
+                throw ("Session expired: please try logging-in again");
             }
             else {
                 return response.text();
             }
-            
+
         } else {
-            throw("You must be logged in to the CAS before fetching a page \n Use login(username, password) to login");
+            throw ("You must be logged in to the CAS before fetching a page \n Use login(username, password) to login");
         }
     }
 
